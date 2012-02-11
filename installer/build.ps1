1c545a6d31e67535d1dec7e260edd66f9141daa6
John Kane <john.kane@spartan-solutions.com>
2012-02-11 10:23:12 +0000

    Generalised to allow specifying particular release.

--- installer/build.ps1
+++ installer/build.ps1
@@ -8,25 +8,38 @@ function generate-wixfragment-from-dir($sourcedir, $targetdir) {
   $componentGroup = $capitalisedTargetDir + "ComponentGroup"
   $directoryVar = $targetdir + "Dir"
 
-  & $wixheat dir .\$sourcedir -nologo -srd -gg -sfrag -template fragment -cg $componentGroup -dr $directoryVar -var var.$directoryVar -out tmp\$targetdir.wxs 
+  & $wixheat dir .\$sourcedir -nologo -srd -gg -sfrag -template fragment -cg $componentGroup -dr $directoryVar -var var.$directoryVar -out tmp\$targetdir.wxs | out-null
 }
 
-# Create a temporary folder for intermediary wix outputs
-mkdir -force tmp | out-null
+# Assumes there has already been a download using ant
+function New-Release($release) {
 
-# Copy or generate all the wix fragments that will be used to build the installer
-# into the tmp directory
-cp .\installer\dejour.wxs .\tmp\dejour.wxs
-cp .\installer\License.rtf .\tmp\License.rtf
-cp -r .\installer\images .\tmp\images
-generate-wixfragment-from-dir -sourcedir bin -targetdir bin
-generate-wixfragment-from-dir -sourcedir examples -targetdir examples
-generate-wixfragment-from-dir -sourcedir "downloads/complete-1.3.0" -targetdir lib
+  # Create a temporary folder for intermediary wix outputs
+  mkdir -force tmp | out-null
 
-# Build the msi using wix
-& $wixcandle -nologo .\tmp\*.wxs -o .\tmp\ -dbinDir=bin -dexamplesDir=examples "-dlibDir=downloads/complete-1.3.0" -dsyslibDir=downloads/jline
+  # Copy or generate all the wix fragments that will be used to build the installer
+  # into the tmp directory
+  cp .\installer\dejour.wxs .\tmp\dejour.wxs
+  cp .\installer\License.rtf .\tmp\License.rtf
+  cp -r .\installer\images .\tmp\images
+  generate-wixfragment-from-dir -sourcedir bin -targetdir bin
+  generate-wixfragment-from-dir -sourcedir examples -targetdir examples
+  generate-wixfragment-from-dir -sourcedir "downloads/$release" -targetdir lib
 
-& $wixlight -nologo .\tmp\*.wixobj -o .\build\dejour.msi -ext $wixUIExtentsion
+  # Build the msi using wix
+  & $wixcandle -nologo .\tmp\*.wxs -o .\tmp\ -dbinDir=bin -dexamplesDir=examples "-dlibDir=downloads/complete-1.3.0" -dsyslibDir=downloads/jline
 
-# Remove the no longer needed temporary folder
-rm -r tmp | out-null
+  & $wixlight -nologo .\tmp\*.wixobj -o .\build\dejour-$release.msi -ext $wixUIExtentsion | out-null
+
+  # Remove unneeded pdb files
+  rm build\*.wixpdb 
+
+  # Remove the no longer needed temporary folder
+  rm -r tmp | out-null
+
+}
+
+New-Release "minimal-1.2.1"
+New-Release "complete-1.2.1"
+New-Release "minimal-1.3.0"
+New-Release "complete-1.3.0"
